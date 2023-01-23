73dc3ec5dbb35dc78556c3d2aa63146bb309e669
Roman Nevzorov <PomkaNevzorov@gmail.com>
2023-01-24 02:33:04 +0600

    Added a lot of code comments and explanations.

--- CurrencyExchangeBot/extensions.py
+++ CurrencyExchangeBot/extensions.py
@@ -1,48 +1,52 @@
 import requests
 import json
 
+# импорт API-ключа и словаря валют
 from CurrencyExchangeBot.config import api_key, currencies
 
 
+# кастомный класс для вызова будущих собственных исключений
 class APIException(Exception):
     pass
 
 
 class Converter:
+    """Класс для создания статического метода для вытаскивания
+    итогового значения конвертации валют."""
+
     @staticmethod
     def get_price(base, quote, amount):
-
+        # если не нашли совпадений по первой введённой валюте
         try:
             base_key = currencies[str(base.lower())]
         except KeyError:
             raise APIException(f"Валюта под названием {base} не найдена!")
-
+        # аналогично для второй валюты
         try:
             quote_key = currencies[str(quote.lower())]
         except KeyError:
             raise APIException(f"Валюта под названием {quote} не найдена!")
-
+        # если валюты совпали
         if base_key == quote_key:
             raise APIException(f"Вы конвертируете две одинаковых валюты!")
-
+        # если третьим значением ввели не (совсем) число
         try:
-            amount = float(amount)
+            amount = float(amount.replace(",", "."))
         except ValueError:
             raise APIException(f"Некорректное значение количества валюты {amount}!")
-
-        # try:
-        #     amount = float(amount.replace(",", "."))
-        # except ValueError:
-        #     raise APIException(f"Не удалось обработать количество валюты {amount}!")
-
+        # если ввели ноль
         if not amount:
             raise APIException(f"Вы ничего не конвертируете!")
-
+        # если ввели отрицательное количество
         if amount <= 0:
             raise APIException(f"Нельзя конвертировать отрицательное количество валюты!")
-
+        # если все проверки пройдены, то формируем запрос,
+        # подставляя API-ключ, и введённые значения
         req = requests.get(f"https://v6.exchangerate-api.com/v6/{api_key}/pair/"
                            f"{base_key}/{quote_key}/{amount}")
+        # по формулам не будем считать - дёргаем результат конвертации напрямую
+        # из результата нашего запроса по нужному ключу - conversion_result
         result = json.loads(req.content)["conversion_result"]
+        # формируем строку-сообщение и возвращаем её как результат метода
         message = f"{amount} {base} = {result} {quote}."
         return message
--- CurrencyExchangeBot/main.py
+++ CurrencyExchangeBot/main.py
@@ -1,6 +1,7 @@
 import telebot
 import traceback
 
+# импортируем наш бот-токен и остальное
 from CurrencyExchangeBot.config import TOKEN
 from CurrencyExchangeBot.extensions import *
 
@@ -11,24 +12,31 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=["start"])
 def start(message: telebot.types.Message):
+    """Обработчик сообщения команды /start."""
     text = "Приветствую Вас!\n\n" \
            "Введите команду /help чтобы увидеть инструкцию по использованию данного бота."
-    bot.send_message(message.chat.id, text)
+    bot.reply_to(message, text)
 
 
 @bot.message_handler(commands=["help"])
 def get_help(message: telebot.types.Message):
-    text = "Команда /values - просмотреть список доступных для конвертации валют" \
-           "и их правильное написание.\n\n" \
-           "Чтобы воспользоваться конвертором валют, введите сообщение в следующем формате:\n\n" \
-           "<название исходной валюты>(пробел)<название конечной валюты>" \
-           "(пробел)<количество исходной валюты>."
+    """Обработчик сообщения команды /help."""
+    text = 'Команда /values - просмотреть список доступных для конвертации валют'\
+           'и их правильное написание.\n\n'\
+           'Чтобы воспользоваться конвертором валют, введите сообщение в следующем формате:\n'\
+           '<название исходной валюты>(пробел)<название конечной валюты>'\
+           '(пробел)<количество исходной валюты>.\n\n'\
+           'Чтобы бот работал корректно, используйте наименования валют из списка /values, '\
+           'вне зависимости от количества.\n'\
+           'Пример: "15 рубль" - верно, "15 рублей" - неверно.'
     bot.reply_to(message, text)
 
 
 @bot.message_handler(commands=["values"])
 def values(message: telebot.types.Message):
-    text = "Доступные валюты:\n"
+    """Обработчик сообщения команды /values."""
+    text = "Доступные валюты и их правильное написание для данного бота:\n"
+    # с помощью цикла выдаём список
     for key in currencies.keys():
         text = "\n".join((text, key))
     bot.reply_to(message, text)
@@ -36,17 +44,24 @@ def values(message: telebot.types.Message):
 
 @bot.message_handler(content_types=["text"])
 def exchange(message: telebot.types.Message):
-    data = message.text.split(" ")
+    """Обработчик сообщения по шаблону команды запроса конвертации."""
+    # делим сообщение на раздельные строки
+    data = message.text.split()
     try:
+        # и если этих строк не 3
         if len(data) != 3:
+            # то сабж
             raise APIException("Неверное количество параметров!")
+        # но если их 3, то они становятся аргументами для нашего статического метода get_price
         answer = Converter.get_price(*data)
+        # отлавливаем наши и не наши исключения и выводим их пользователю
     except APIException as e:
         bot.reply_to(message, f"Ошибка в команде:\n{e}")
     except Exception as e:
         traceback.print_tb(e.__traceback__)
         bot.reply_to(message, f"Неизвестная ошибка:\n{e}")
     else:
+        # и если всё ок, отправляем ответ пользователю в виде сообщения
         bot.reply_to(message, answer)
 
 
